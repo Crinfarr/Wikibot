a17f7b1e31249b926d86fe3178fbfe4af6050eda
Crinfarr <31548109+Crinfarr@users.noreply.github.com>
2021-03-08 14:12:18 -0600

    add status messages and invite link

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
     console.log('logged in as ' + client.user.tag)
+    client.user.setPresence({
+        activity: {
+            name: 'for --wiki',
+            type: 'LISTENING',
+            url: 'https://discord.com/oauth2/authorize?client_id=527923239050149898&scope=bot&permissions=52224'
+        },
+        afk: false,
+        status: 'online'
+    })
 });
 
 async function sendarticle(msg, args) {
@@ -41,11 +50,16 @@ async function sendarticle(msg, args) {
 client.on('message', msg => {
     if (msg.author.bot) return;
     if (!msg.content.startsWith('--')) return;
-    args = msg.content.split(' ')
+    args = msg.content.split(' ');
     switch (args[0]) {
         case '--wiki':
+            if (args.length < 2) {
+                msg.channel.send('usage: --wiki [search terms]')
+                console.log(`${msg.author.tag} got help at ${new Date()}`);
+                return;
+            }
             sendarticle(msg, args);
-            console.log(`${msg.author.tag} called for an article on ${msg.content.slice(1).join(' ')} at ${new Date()}`)
+            console.log(`${msg.author.tag} called for an article on ${msg.content.slice(1).join(' ')} at ${new Date()}`);
     }
 })
 
